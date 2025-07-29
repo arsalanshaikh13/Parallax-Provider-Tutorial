2299be46d315fe38831d1c899e869c7183f5c726
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-07-29 21:14:22 +0530

    prettier messign with inline scripts so created shell file ci-scripts/ abstracte the shell logic to .sh file and combined git push and triggerlogic

---
+++ ci-scripts/gitlab-sync-and-trigger.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Setting up Git configuration..."
+git config --global user.email "ci@example.com"
+git config --global user.name "CI user"
+
+echo "Adding Gitlab remote"
+git remote add gitlab "https://oauth2:${GITLAB_PUSH_TOKEN}@gitlab.com/arsalanshaikh13/Parallax-Provider-Tutorial.git"
+
+echo "Pushing latest commit to Gitlab branch: ${GITLAB_BRANCH}"
+git push gitlab HEAD:${GITLAB_BRANCH}
+
+echo " Triggering Gitlab pipeline..."
+curl -X POST \
+    -H "PRIVATE-TOKEN: ${GITLAB_PAT}" \
+    -H "Content-Type: application/json" \
+    -d "{\"ref\":\"${GITLAB_BRANCH}\"}" \
+    "https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/pipeline"
