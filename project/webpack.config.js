c95943b80233bda1ce4c95e28c29f6c3f2dac34a
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-01 20:10:36 +0530

    change extension of index.js to index.mjs use ESM syntax of export and import and also created index.cjs file to use internally the index.mjs code with cjs syntax, updated webpack.config.js to resolve for mjs,js,json file so there is no change to import syntax under project/src/main.js file, updated rollup.config.mjs and .js file to output esm.js instead of faulty es.js, updated package.json to define public api export/output of import/require/browser under exports tag which will be out export the necessary js file on the relevant import functions like import,require,script in browser

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     filename: 'bundle.js',
     // publicPath: "/", // required for dev server routing
   },
+  resolve: {
+    extensions: ['.mjs', '.js', '.json'],
+  },
   mode: 'development',
   devServer: {
     static: {
