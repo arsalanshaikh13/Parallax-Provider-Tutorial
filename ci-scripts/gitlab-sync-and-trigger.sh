098ccbf0cd772038d66b2db46e687e1ebcbd2a79
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-02 09:20:59 +0530

    change if condition to check for tag in github context through  instead of error checking

--- ci-scripts/gitlab-sync-and-trigger.sh
+++ ci-scripts/gitlab-sync-and-trigger.sh
@@ -9,36 +9,24 @@ echo "Adding Gitlab remote"
 git remote add gitlab "https://oauth2:${GITLAB_PUSH_TOKEN}@gitlab.com/arsalanshaikh13/Parallax-Provider-Tutorial.git"
 
 
-LATEST_TAG=$(git describe --tags 2>/dev/null || echo "")
-echo " Latest Tag is : $LATEST_TAG and pushing it to gitlab"
 
-set +e # do not exit immediately on error below
-echo "Triggering Gitlab pipeline..."
-ERROR_VAR=$(git push  gitlab $LATEST_TAG 2>&1)
-echo "ERROR_VAR is $ERROR_VAR"
-EXIT_CODE=$?
-echo "exit code is $EXIT_CODE"
-set -e # if there is any error in below lines exit immediately
-
-if [[  "$ERROR_VAR" != *"error:"* ]]; then
-    echo "Push succeeded"
+if [["$GITHUB_REF_TYPE" == "tag"]]; then
+    echo "tag found"
     echo "Waiting for GitLab to register the tag..."
+    LATEST_TAG="${GITHUB_REF#refs/tags/}"
+    git push  gitlab $LATEST_TAG
     sleep 5
     echo "Triggering Gitlab pipeline on tag push..."
-    
-    
     curl -X POST \
         -H "PRIVATE-TOKEN: ${GITLAB_PAT}" \
         -H "Content-Type: application/json" \
         -d "{\"ref\":\"$LATEST_TAG\"}" \
         "https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/pipeline"
-elif ["$GITHUB_REF_TYPE" == "tag"]; then
-    echo "found existing tag "
-    exit 1
+
 else
     echo "No tags found"
-    echo "Force pushing latest commit GitHub code to GitLab..."
-    git push --force  gitlab HEAD:${GITLAB_BRANCH}
+    echo " pushing latest commit GitHub code to GitLab..."
+    git push  gitlab HEAD:${GITLAB_BRANCH}
 
     echo "Triggering Gitlab pipeline on branch push..."
     curl -X POST \
