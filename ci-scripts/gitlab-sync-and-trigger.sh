8ae78cb7ea18ef32f67d56f58345d22ea5fec7b6
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-05 14:16:43 +0530

    change set -e condition with set +e on the git diff section from trigger.sh to not exit immediately after error  exit onl and reduce sleep to 1s

--- ci-scripts/gitlab-sync-and-trigger.sh
+++ ci-scripts/gitlab-sync-and-trigger.sh
@@ -7,12 +7,14 @@ echo "$GITHUB_REF_TYPE  is github_ref_type and $GITHUB_REF  is github_Ref and ${
 
 git remote add gitlab "https://oauth2:${GITLAB_PUSH_TOKEN}@gitlab.com/arsalanshaikh13/Parallax-Provider-Tutorial.git"
 
-
+# git diff section
+# don't exit immediately in this operation since we want to capture the error to display the message
+set +e
 PREVIOUS_COMMIT=$(git rev-parse HEAD~1)
 echo "COMMIT_SHA: $GITHUB_SHA"
 echo "COMMIT_BEFORE_SHA: $PREVIOUS_COMMIT"
 
-# CHANGED=$(git show --pretty="" --name-only "$CI_COMMIT_SHA"  | grep -E '\.js$|\.json$|\.yml$|\..*rc$' 2>/dev/null)
+# CHANGED=$(git show --pretty="" --name-only "$GITHUB_SHA"  | grep -E '\.js$|\.json$|\.yml$|\..*rc$' 2>/dev/null)
 
 CHANGED=$(git diff --name-only "$PREVIOUS_COMMIT" "$GITHUB_SHA"  | grep -E '\.js$|\.json$|\.yml$|\..*rc$' 2>/dev/null)
 if [ -z "$CHANGED" ]; then
@@ -20,13 +22,15 @@ echo "No relevant changes found. Stopping pipeline."
 exit 1
 fi
 echo "Found relevant changes: $CHANGED"
+# now if there is any error below exit immediately
+set -e
 
 if [[ "$GITHUB_REF_TYPE" == "tag" ]]; then
     echo "tag found"
     echo "Waiting for GitLab to register the tag..."
     LATEST_TAG="${GITHUB_REF#refs/tags/}"
     git push  gitlab $LATEST_TAG
-    sleep 2
+    sleep 1
     echo "Triggering Gitlab pipeline on tag push..."
     curl -X POST \
         -H "PRIVATE-TOKEN: ${GITLAB_PAT}" \
