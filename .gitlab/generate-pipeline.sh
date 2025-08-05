452feaa36a7359734b4691f7ba11f5492b2712d7
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-06 01:56:44 +0530

    add .sh in the regex pattern inside diff condition in generate-pipeline.sh file and removed the debugging lines from yml and sh files

--- .gitlab/generate-pipeline.sh
+++ .gitlab/generate-pipeline.sh
@@ -4,9 +4,9 @@ set +e # gitlabci exits immediately by default due set -e so reverse the exit co
 if [ "$CI_COMMIT_BEFORE_SHA" = "0000000000000000000000000000000000000000" ] ||
     ! git cat-file -e "$CI_COMMIT_BEFORE_SHA" >/dev/null 2>&1; then
 
-    CHANGED=$(git show --pretty="" --name-only "$CI_COMMIT_SHA" 2>&1 | grep -E '\.js$|\.json$|\.yml$|\.lock$|\..*rc$' )
+    CHANGED=$(git show --pretty="" --name-only "$CI_COMMIT_SHA" 2>&1 | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
 else
-    CHANGED=$(git diff --pretty="" --name-only "$CI_COMMIT_BEFORE_SHA" "$CI_COMMIT_SHA" 2>&1  | grep -E '\.js$|\.json$|\.yml$|\.lock$|\..*rc$' )
+    CHANGED=$(git diff --pretty="" --name-only "$CI_COMMIT_BEFORE_SHA" "$CI_COMMIT_SHA" 2>&1  | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
 fi
 echo "output of diff: $CHANGED "
 if [ -z "$CHANGED" ]; then
@@ -15,12 +15,10 @@ if [ -z "$CHANGED" ]; then
 include:
   - local: '.gitlab/ci-empty.yml'
 EOF
-        echo "Reached end of condition block"
 else
     echo "Found relevant changes: $CHANGED"
     cat > .gitlab/pipeline-config.yml <<EOF
 include:
   - local: '.gitlab/ci-full.yml'
 EOF
-    echo "Reached end of condition block"
 fi
