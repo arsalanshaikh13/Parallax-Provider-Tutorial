099abd5629c926758a1641edc6935c8ec9a021ee
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-06 01:20:58 +0530

    put all the script logic inside check for file changes into .gitlab/generate-pipelin.sh file

---
+++ .gitlab/generate-pipeline.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set +e # gitlabci exits immediately by default due set -e so reverse the exit condition here to capture error
+# initalize the bool variable
+if [ "$CI_COMMIT_BEFORE_SHA" = "0000000000000000000000000000000000000000" ] ||
+    ! git cat-file -e "$CI_COMMIT_BEFORE_SHA" >/dev/null 2>&1; then
+
+    CHANGED=$(git show --pretty="" --name-only "$CI_COMMIT_SHA" 2>&1 | grep -E '\.js$|\.json$|\.yml$|\.lock$|\..*rc$' )
+else
+    CHANGED=$(git diff --pretty="" --name-only "$CI_COMMIT_BEFORE_SHA" "$CI_COMMIT_SHA" 2>&1  | grep -E '\.js$|\.json$|\.yml$|\.lock$|\..*rc$' )
+fi
+echo "output of diff: $CHANGED "
+if [ -z "$CHANGED" ]; then
+    echo "No relevant changes found. Generating empty pipeline."
+    cat  > .gitlab/pipeline-config.yml <<EOF
+include:
+  - local: '.gitlab/child-empty.yml'
+EOF
+        echo "Reached end of condition block"
+else
+    echo "Found relevant changes: $CHANGED"
+    cat > .gitlab/pipeline-config.yml <<EOF
+include:
+  - local: '.gitlab/child-full.yml'
+EOF
+    echo "Reached end of condition block"
+fi
