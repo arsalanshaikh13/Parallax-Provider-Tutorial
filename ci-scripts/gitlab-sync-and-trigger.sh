451dd6575de24a2ce76ee0b446d6d38b677130ac
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-29 13:02:44 +0530

    updated GITLAB_PAT with new personal access token  having read and write access to repo for private project over http

--- ci-scripts/gitlab-sync-and-trigger.sh
+++ ci-scripts/gitlab-sync-and-trigger.sh
@@ -26,7 +26,6 @@ if [[ "$GITHUB_REF_TYPE" == "tag" ]]; then
 else
     echo "No tags found"
     echo " pushing latest commit GitHub code to GitLab..."
-    echo "${GITLAB_PAT} gitlab pat"
     git push --force gitlab HEAD:${GITLAB_BRANCH}
 
     echo "Triggering Gitlab pipeline on branch push..."
