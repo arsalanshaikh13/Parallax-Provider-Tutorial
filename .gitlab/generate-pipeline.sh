34ac6662d089fb7df47ad144cb688962579307bb
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-29 22:17:47 +0530

    since we are passing ci_commit_before_sha as variable then only using git diff in generate-pipeline.sh and removing all the if conditions and git show

--- .gitlab/generate-pipeline.sh
+++ .gitlab/generate-pipeline.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 set +e # gitlabci exits immediately by default due set -e so reverse the exit condition here to capture error
 # initalize the bool variable
-if [ "$CI_COMMIT_BEFORE_SHA" = "0000000000000000000000000000000000000000" ] ||
-    ! git cat-file -e "$CI_COMMIT_BEFORE_SHA" >/dev/null 2>&1; then
+# if [  "$CI_COMMIT_BEFORE_SHA" = "0000000000000000000000000000000000000000" ] ||
+#     ! git cat-file -e "$CI_COMMIT_BEFORE_SHA" >/dev/null 2>&1; then
 
-    CHANGED=$(git show --pretty="" --name-only "$CI_COMMIT_SHA" 2>&1 | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
-    echo "used git show"
-else
-    CHANGED=$(git diff --pretty="" --name-only "$CI_COMMIT_BEFORE_SHA" "$CI_COMMIT_SHA" 2>&1  | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
-    echo "used git diff with variable using from api commit before sha -> $CI_COMMIT_BEFORE_SHA and current commit $CI_COMMIT_SHA"
-fi
+#     CHANGED=$(git show --pretty="" --name-only "$CI_COMMIT_SHA" 2>&1 | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
+#     echo "used git show"
+# else
+#     CHANGED=$(git diff --pretty="" --name-only "$CI_COMMIT_BEFORE_SHA" "$CI_COMMIT_SHA" 2>&1  | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
+#     echo "used git diff with variable using from api commit before sha -> $CI_COMMIT_BEFORE_SHA and current commit $CI_COMMIT_SHA"
+# fi
+CHANGED=$(git diff --pretty="" --name-only "$CI_COMMIT_BEFORE_SHA" "$CI_COMMIT_SHA" 2>&1  | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
+echo "used git diff with variable using from api commit before sha -> $CI_COMMIT_BEFORE_SHA and current commit $CI_COMMIT_SHA"
+
 echo "output of diff: $CHANGED "
 if [ -z "$CHANGED" ]; then
     echo "No relevant changes found. Generating empty pipeline."
