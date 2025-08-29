68dff767c402b4b3e84a28339ab5860c7d31b102
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2025-08-29 21:40:40 +0530

    added echo statement to print git show or git diff in generate-pipeline.sh file

--- .gitlab/generate-pipeline.sh
+++ .gitlab/generate-pipeline.sh
@@ -5,8 +5,10 @@ if [ "$CI_COMMIT_BEFORE_SHA" = "0000000000000000000000000000000000000000" ] ||
     ! git cat-file -e "$CI_COMMIT_BEFORE_SHA" >/dev/null 2>&1; then
 
     CHANGED=$(git show --pretty="" --name-only "$CI_COMMIT_SHA" 2>&1 | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
+    echo "used git show"
 else
     CHANGED=$(git diff --pretty="" --name-only "$CI_COMMIT_BEFORE_SHA" "$CI_COMMIT_SHA" 2>&1  | grep -E '\.js$|\.json$|\.yml$|\.lock$|\.sh$|\..*rc$' )
+    echo "used git diff with variable using from api commit before sha -> $CI_COMMIT_BEFORE_SHA and current commit $CI_COMMIT_SHA"
 fi
 echo "output of diff: $CHANGED "
 if [ -z "$CHANGED" ]; then
